fe06c4f649041310ad98e08eb4785ecc5d48f561
Drknyt199711 <fromsadawit@gmail.com>
2025-06-20 00:34:33 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ const urlsToCache = [
     '/style.css',
     '/script.js',
     '/manifest.json',
-    '/images/icon-192x192.png',
-    '/images/icon-512x512.png'
+    '/icon-192x192.png',
+    '/icon-512x512.png'
 ];
 
 // Install event: Caches all assets specified in urlsToCache
